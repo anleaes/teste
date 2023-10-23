4728f4b4d85a06ea9ce01ad92f21278354f330db
Gustavo Santos <146127015+gustavosantos04@users.noreply.github.com>
2023-10-23 20:25:43 -0300

    criando função

--- livro.py
+++ livro.py
@@ -1,4 +1,13 @@
 class Livro:
 
-    def __init__(self, titulo):
+    def __init__(self, titulo, snopse, pagina, autor):
         self._titulo = titulo 
+        self._snopse = snopse
+        self._pagina = pagina 
+        self._autor = autor
+
+    def imprime_dados():
+        print(titulo, snopse, pagina, autor)
+imprime_dados()    
+
+    
