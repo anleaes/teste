1133329202f987fefa425f14f50e19d6db296a4f
anleaes <anleaes@gmail.com>
2023-10-23 21:02:53 -0300

    dfsadsa

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+from autor import Autor
+from livro import Livro
 
-
-def main():    
-    
+def main():
+    a1 = Autor('Antonio Leaes', 1982, 'a@a.com')
+    a2 = Autor('Luiz Silva', 1990, 'l@l.com')
+    l1 = Livro('Senhor dos Aneis', 'Aventura', 8000, a1)
     
 if __name__ == "__name__":
     main
